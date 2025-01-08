daae88af40ad1c6b61c020a6bd75723dfca52ae4
ALP300 <aitorpalomino1@gmail.com>
2025-01-08 18:34:33 -0500

    Crear proyecto

---
+++ src/main/java/com/mycompany/curso_java_enero/Curso_Java_Enero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.curso_java_enero;
+
+/**
+ *
+ * @author aitor
+ */
+public class Curso_Java_Enero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
