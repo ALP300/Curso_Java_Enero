030788b23a329905232cf1dde7b8ee16891e6073
AndyFM806 <leefernandez1245@gmail.com>
2025-01-08 18:57:43 -0500

    commit3

--- src/main/java/com/mycompany/curso_java_enero/Curso_Java_Enero.java
+++ src/main/java/com/mycompany/curso_java_enero/Curso_Java_Enero.java
@@ -12,5 +12,6 @@ public class Curso_Java_Enero {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Curso java enero");
     }
 }
